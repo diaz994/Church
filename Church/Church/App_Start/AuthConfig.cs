d813859a3cc6e4e0af271c7ceb0fe5d218aa7392
Victor Diaz <vsdiaz@DALLAP8560801CG.dfs.com>
2013-08-06 10:46:19 -0500

    Initial commit

---
+++ Church/Church/App_Start/AuthConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using Church.Models;
+
+namespace Church
+{
+	public static class AuthConfig
+	{
+		public static void RegisterAuth()
+		{
+			//OAuthWebSecurity.RegisterMicrosoftClient(
+			//    clientId: "",
+			//    clientSecret: "");
+
+			//OAuthWebSecurity.RegisterTwitterClient(
+			//    consumerKey: "",
+			//    consumerSecret: "");
+
+			OAuthWebSecurity.RegisterFacebookClient(
+				appId: "573370666038574",
+				appSecret: "a3e1b3a2ab1129ba9cd80a35dd643e31");
+
+			//OAuthWebSecurity.RegisterGoogleClient();
+		}
+	}
+}
